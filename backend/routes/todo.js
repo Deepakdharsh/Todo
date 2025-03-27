8a3d5453ac7bac956b2a044f1a31b946679fe743
Deepak <deepakdharsh01@gmail.com>
2025-03-27 21:35:21 +0530

    modified code

--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -85,8 +85,6 @@ TodoRouter.get("/read",auth,async(req,res)=>{
 
 TodoRouter.delete("/delete/:id",auth,async(req,res)=>{
     const userId=req.userId
-    // const {todoId}=req.body
-    // console.log(req.body)
     const todoId=req.params.id
     
     if(!todoId){
